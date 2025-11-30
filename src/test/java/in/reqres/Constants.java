d42a1817d67ac950b3a027d1e00cfd3f86f0df94
annashelyakina <anna.shelyakina82@gmail.com>
2025-11-30 12:02:59 +0300

    updated names in Constants

--- src/test/java/in/reqres/Constants.java
+++ src/test/java/in/reqres/Constants.java
@@ -7,8 +7,8 @@ public class Constants {
     public static final Header VALID_API_KEY = new Header("x-api-key", "reqres-free-v1");
     public static final Header INVALID_API_KEY = new Header("x-api-key", "reqres-free-v111");
     public static final String VALID_EMAIL = "eve.holt@reqres.in";
-    public static final String VALID_PASSWORD = "pistol";
     public static final String INVALID_EMAIL = "eve111.holt@reqres.in";
+    public static final String VALID_PASSWORD = "pistol";
     public static final String VALID_NAME = "morpheus";
     public static final String VALID_JOB = "zion resident";
  }
